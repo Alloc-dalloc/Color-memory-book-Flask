bad2dee8eb93bc810f06d86cf45b72ac7b4a0efa
99winnmin <ojysep9987@gmail.com>
2023-05-24 16:13:14 +0900

    [Feat] Flask init setting

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from color_analysis import ColorAnalyzer
+
+app = Flask(__name__)
+analyzer = ColorAnalyzer()
+
+
+@app.route('/color', methods=['POST'])
+def color_analysis():
+    if request.method == 'POST':
+        data = request.json
+        if 'image' not in data:
+            return "No image URL in the request."
+        image_url = data['image']
+        analyzed_color = analyzer.analyze_color(image_url)
+        return analyzed_color
+
+
+if __name__ == '__main__':
+    app.run()
